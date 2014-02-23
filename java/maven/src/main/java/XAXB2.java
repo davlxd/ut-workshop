d34872f32854fc0ff587f0d2a63f5e135265f09c
lxd <lxd@Air>
2014-02-23 14:48:49 +0800

    lxd - Add naive implementation, make test pass

--- java/maven/src/main/java/XAXB2.java
+++ java/maven/src/main/java/XAXB2.java
@@ -2,7 +2,13 @@
  * Created by lxd on 23/2/14.
  */
 public class XAXB2 {
-    public String count(String s, String s1) {
-        return null;
+    public String count(String challenge, String guess) {
+        int count = 0;
+        for (int i = 0; i < challenge.length(); i++) {
+            if (challenge.charAt(i) == guess.charAt(i)) {
+                count += 1;
+            }
+        }
+        return count + "A" + "0B";
     }
 }

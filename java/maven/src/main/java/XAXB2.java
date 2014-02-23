3fc1553bd131d54e153460e1ce290a9f863b0005
lxd <lxd@Air>
2014-02-23 15:06:51 +0800

    lxd - Refactor

--- java/maven/src/main/java/XAXB2.java
+++ java/maven/src/main/java/XAXB2.java
@@ -2,22 +2,33 @@
  * Created by lxd on 23/2/14.
  */
 public class XAXB2 {
-    public String count(String challenge, String guess) {
-        int countA = 0, countB = 0;
+
+    public int countA(String challenge, String guess) {
+        int count = 0;
         for (int i = 0; i < challenge.length(); i++) {
             if (challenge.charAt(i) == guess.charAt(i)) {
-                countA += 1;
+                count += 1;
             }
         }
+        return count;
+    }
 
+    public int countB(String challenge, String guess) {
+        int count = 0;
         for (int i = 0; i < guess.length(); i++) {
             for (int j = 0; j < challenge.length(); j++) {
                 if (guess.charAt(i) == challenge.charAt(j)) {
-                    countB += 1;
+                    count += 1;
                 }
             }
         }
+        return count;
+    }
 
-        return countA + "A" + (countB - countA) + "B";
+    public String count(String challenge, String guess) {
+        int count_a = countA(challenge, guess);
+        int count_b = countB(challenge, guess);
+
+        return count_a + "A" + (count_b - count_a) + "B";
     }
 }

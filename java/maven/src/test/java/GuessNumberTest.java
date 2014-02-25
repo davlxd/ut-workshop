7a9a6c918ac8ea191f786425ead196b8de3e22ba
lxd <xdli@thoughtworks.com>
2014-02-25 13:26:01 +0800

    lxd - Add integrate test; Refactor

--- java/maven/src/test/java/GuessNumberTest.java
+++ java/maven/src/test/java/GuessNumberTest.java
@@ -11,12 +11,14 @@ import static org.mockito.Mockito.when;
  * Created by xdli on 2/25/14.
  */
 public class GuessNumberTest {
-    private GuessNumber guessNumber;
 
     @Before
     public void setup() {
     }
 
+    /**
+     * Unit integrate test
+     */
     @Test
     public void mock_both_RandomNumberGenerate_and_CompareTwoNumbers_should_work() {
         RandomNumberGenerator randomNumberGenerator = mock(RandomNumberGenerator.class);
@@ -25,22 +27,24 @@ public class GuessNumberTest {
         CompareTwoNumbers compareTwoNumbers = mock(CompareTwoNumbers.class);
         when(compareTwoNumbers.countAB("1234", "1234")).thenReturn("4A0B");
 
-        guessNumber = new GuessNumber(compareTwoNumbers, randomNumberGenerator);
+        GuessNumber guessNumber = new GuessNumber(compareTwoNumbers, randomNumberGenerator);
 
         assertThat(guessNumber.guess("1234")).isEqualTo("4A0B");
-
     }
 
-    @Test
-    public void mock_only_RandomNumberGenerate_should_work() {
-        RandomNumberGenerator randomNumberGenerator = mock(RandomNumberGenerator.class);
-        when(randomNumberGenerator.generate()).thenReturn("1234").thenReturn("2345");
 
+    /**
+     * Real integrate test
+     */
+    @Test
+    public void integrate_test_RandomNumberGenerate_and_CompareTwoNumbers_should_work() {
+        Random random = mock(Random.class);
+        when(random.nextInt(10)).thenReturn(1).thenReturn(2).thenReturn(3).thenReturn(4);
+
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(random);
         CompareTwoNumbers compareTwoNumbers = new CompareTwoNumbers();
 
-        guessNumber = new GuessNumber(compareTwoNumbers, randomNumberGenerator);
-
+        GuessNumber guessNumber = new GuessNumber(compareTwoNumbers, randomNumberGenerator);
         assertThat(guessNumber.guess("1234")).isEqualTo("4A0B");
-        assertThat(guessNumber.guess("1234")).isEqualTo("0A3B");
     }
 }

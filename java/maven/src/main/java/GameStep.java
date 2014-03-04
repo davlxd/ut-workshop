204d23ca0fc3f23bd03c1a04f6bb7b3f7aa86c52
lxd <i@lxd.me>
2014-03-04 08:26:00 +0800

    Add more test case for GameStep and make them pass

--- java/maven/src/main/java/GameStep.java
+++ java/maven/src/main/java/GameStep.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /**
  * Created by lxd on 2/3/14.
  */
@@ -10,8 +12,33 @@ public class GameStep {
         this.readUserInput = readUserInput;
     }
 
-    public String step() {
+    private boolean user_input_is_valid(String str) {
+        if (str.length() != 4) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) < '0' || str.charAt(i) > '9') {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < str.length() && i != j; j++) {
+                if (str.charAt(i) == str.charAt(j))
+                    return false;
+            }
+        }
+
+
+        return true;
+    }
+
+    public String step() throws IOException {
         String userInput = readUserInput.readLine();
+        if (!user_input_is_valid(userInput)) {
+            return "";
+        }
         return guessNumber.guess(userInput);
     }
 }
--- java/maven/src/test/java/GameStepTest.java
+++ java/maven/src/test/java/GameStepTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,20 +17,43 @@ public class GameStepTest {
 
     @Before
     public void setup() {
-    }
-
-    @Test
-    public void if_guess_and_user_input_matches_return_4A0B() {
         guessNumber = mock(GuessNumber.class);
         readUserInput = mock(ReadUserInput.class);
 
         gameStep = new GameStep(guessNumber, readUserInput);
+    }
 
+    @Test
+    public void if_guess_and_user_input_matches_return_4A0B() throws IOException {
         when(guessNumber.guess("1234")).thenReturn("4A0B");
         when(readUserInput.readLine()).thenReturn("1234");
 
         String ret = gameStep.step();
         assertThat(ret).isEqualTo("4A0B");
+    }
+
+    @Test
+    public void will_return_empty_if_user_input_invalid() throws IOException {
+        when(readUserInput.readLine()).thenReturn("12324");
+
+        String ret = gameStep.step();
+        assertThat(ret).isEmpty();
+    }
+
+    @Test
+    public void will_return_empty_if_user_input_not_all_invalid() throws IOException {
+        when(readUserInput.readLine()).thenReturn("12id");
+
+        String ret = gameStep.step();
+        assertThat(ret).isEmpty();
+    }
+
+    @Test
+    public void will_return_empty_if_user_input_duplicate_digit() throws IOException {
+        when(readUserInput.readLine()).thenReturn("1223");
+
+        String ret = gameStep.step();
+        assertThat(ret).isEmpty();
     }
 
 

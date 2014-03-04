91e82f06694e132681ddfb7ee21493870655d185
lxd <xdli@thoughtworks.com>
2014-03-04 11:05:04 +0800

    Fill in ReaderUserInput and manual test pass

--- java/maven/src/main/java/ReadUserInput.java
+++ java/maven/src/main/java/ReadUserInput.java
@@ -1,8 +1,24 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by lxd on 3/3/14.
  */
 public class ReadUserInput {
-    public String readLine() {
-        return null;
+    private InputStream inputStream;
+
+    public ReadUserInput(InputStream inputStream) {
+        this.inputStream = inputStream;
     }
+
+    public String readLine() throws IOException {
+        byte[] userInput = new byte[1024];
+        inputStream.read(userInput);
+        return new String(userInput).trim();
+    }
+//
+//    public static void main(String args[]) throws IOException {
+//        String txt =  new ReadUserInput(System.in).readLine();
+//        System.out.println(txt);
+//    }
 }
